040ce4ecb764c0aeff63730536b292590f64261e
Ahmad L. Mahendra <mahendra33115@gmail.com>
2025-11-28 23:25:21 +0700

    Change API base URL to production

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://load-map.vercel.app/";
 let map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
